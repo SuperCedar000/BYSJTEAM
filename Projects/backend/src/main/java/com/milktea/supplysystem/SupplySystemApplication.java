f40d356c2afe464bcfc214d77ab40be7a3395bd0
SuperCedar000 <775255837@qq.com>
2026-03-26 23:26:38 +0800

    Initialize a Spring Boot project and configure basic dependencies
    Create a Maven project and add dependencies such as Spring Boot, JPA, Security, MySQL, JWT, etc. Configure application.properties (database connection, JWT key, port, etc.) Create the main startup

--- Projects/backend/src/main/java/com/milktea/supplysystem/SupplySystemApplication.java
+++ Projects/backend/src/main/java/com/milktea/supplysystem/SupplySystemApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SupplySystemApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SupplySystemApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SupplySystemApplication.class, args);
+    }
 }
