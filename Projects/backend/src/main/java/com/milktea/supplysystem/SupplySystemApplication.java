0cb634965996ee708a8b8806b95229c80c579bc6
WangX6666 <1271159577@qq.com>
2026-04-05 09:35:02 +0800

    1
    1

---
+++ Projects/backend/src/main/java/com/milktea/supplysystem/SupplySystemApplication.java
@@ -0,0 +1,13 @@
+package com.milktea.supplysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplySystemApplication.class, args);
+	}
+
+}
---
+++ Projects/backend/src/test/java/com/milktea/supplysystem/SupplySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milktea.supplysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
